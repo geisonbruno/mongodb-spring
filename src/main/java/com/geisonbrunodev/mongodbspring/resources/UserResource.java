948f667ec998e8a9fc46707202399301f991aa17
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-01-23 14:14:41 -0300

    feat: using DTO pattern to return users

--- src/main/java/com/geisonbrunodev/mongodbspring/resources/UserResource.java
+++ src/main/java/com/geisonbrunodev/mongodbspring/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.geisonbrunodev.mongodbspring.resources;
 
 import com.geisonbrunodev.mongodbspring.domain.User;
+import com.geisonbrunodev.mongodbspring.dto.UserDTO;
 import com.geisonbrunodev.mongodbspring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -17,9 +19,13 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
-        List<User> list = service.findAll();
+    public ResponseEntity<List<UserDTO>> findAll() {
 
-        return ResponseEntity.ok().body(list);
+        List<User> list = service.findAll();
+        List<UserDTO> listDto = list.stream()
+                .map(UserDTO::new)
+                .toList();
+
+        return ResponseEntity.ok().body(listDto);
     }
 }
