a31ea8ea3bc6262fecdac1e6f7eeaa2d525865ed
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-01-29 15:40:10 -0300

    feat: Projection of the author's data with dto

--- src/main/java/com/geisonbrunodev/mongodbspring/dto/AuthorDTO.java
+++ src/main/java/com/geisonbrunodev/mongodbspring/dto/AuthorDTO.java
@@ -1,4 +1,39 @@
 package com.geisonbrunodev.mongodbspring.dto;
 
-public class AuthorDTO {
+import com.geisonbrunodev.mongodbspring.domain.User;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {
+    }
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
