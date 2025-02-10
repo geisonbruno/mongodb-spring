ca01f1e08c122d08648db53a2d3baf0e723425f1
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-02-10 18:53:25 -0300

    feat: consulta com varios criterios

--- src/main/java/com/geisonbrunodev/mongodbspring/resources/util/URL.java
+++ src/main/java/com/geisonbrunodev/mongodbspring/resources/util/URL.java
@@ -1,5 +1,9 @@
 package com.geisonbrunodev.mongodbspring.resources.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class URL {
 
     public static String decodeParam(String text) {
@@ -8,6 +12,18 @@ public class URL {
         } catch (java.io.UnsupportedEncodingException e) {
             return "";
         }
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        try {
+            return sdf.parse(textDate);
+        } catch (java.text.ParseException e) {
+            return defaultValue;
+        }
+
     }
 
 }
